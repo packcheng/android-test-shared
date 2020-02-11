e3bfa2ef1a3b93e9158fa4b678231a943e4a80c7
packcheng <packcheng.zhou@dinsafe.com>
2020-02-11 12:27:56 +0800

    首次提交

--- README.md
+++
@@ -1,2 +0,0 @@
-# android-test-shared
-测试创建implementation 依赖
---
+++ libcommon/src/main/java/com/packcheng/libcommon/TestLogUtil.java
@@ -0,0 +1,13 @@
+package com.packcheng.libcommon;
+
+import android.util.Log;
+
+/**
+ * @author packcheng
+ * @date 2020-02-11 11:27
+ */
+public class TestLogUtil {
+    public static void i(String msg) {
+        Log.i("TAG", msg);
+    }
+}
